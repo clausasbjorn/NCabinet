1c0d8652e2e967440ac56a8620d87325565bfa14
Claus A. Sorensen <claus@braincell.no>
2011-10-18 22:09:29 +0200

    CacheManager proof of concept Created the first wrapper around callbacks, allowing the client to send methods for callbacks when no data is found

--- NCabinet/CacheManager.cs
+++ NCabinet/CacheManager.cs
@@ -2,19 +2,31 @@ using System;
 
 namespace NCabinet
 {
-    public delegate TOut KeyWrapper<TIn1, TIn2, TOut>(TIn1 i1, TIn2 i2);
-
     public class CacheManager : ICacheManager
     {
+        private class NoKey { }
 
-        public TOut Get<TIn1, TIn2, TOut>(Func<TIn1, TIn2, TOut> a, TIn1 i1, TIn2 i2)
+        private static NoKey _n;
+        private static NoKey N
         {
-            return a(i1, i2);
+            get { return _n ?? (_n = new NoKey()); }
         }
 
-        public TOut Get<TIn1, TOut>(Func<TIn1, TOut> a, TIn1 i1)
+        public TOut Get<TIn1, TIn2, TIn3, TOut>(Func<TIn1, TIn2, TIn3, TOut> callback, TIn1 i1, TIn2 i2, TIn3 i3)
         {
-            return Get()
+            return callback(i1, i2, i3);
+        }
+
+        public TOut Get<TIn1, TIn2, TOut>(Func<TIn1, TIn2, TOut> callback, TIn1 i1, TIn2 i2)
+        {
+            Func<TIn1, TIn2, NoKey, TOut> wrapper = (p1, p2, n2) => callback(p1, p2);
+            return Get(wrapper, i1, i2, N);
+        }
+
+        public TOut Get<TIn1, TOut>(Func<TIn1, TOut> callback, TIn1 i1)
+        {
+            Func<TIn1, NoKey, NoKey, TOut> wrapper = (p1, n1, n2) => callback(p1);
+            return Get(wrapper, i1, N, N);
         }
 
         private T Get<T>()
